c668754ebeef18d6eb60e2e5dd05aa05af50297b
Klas Holmgren <klas0holmgren@gmail.com>
2024-03-13 20:06:26 +0100

    make traslations for swedish

--- data/art.py
+++ data/art.py
@@ -8,7 +8,9 @@ wolf_drawing = {
     'description': "A wolf drawn with pencil.",
     'image': 'wolf_drawing.jpg',
     'url': 'wolf_drawing.jpg',
-    'skills': ['Nature', 'gray', 'animal']
+    'skills': ['Nature', 'gray', 'animal'],
+    'titel_sv': 'Vargritning',
+    'description_sv': "En varg ritad med blyertspenna."
 }
 
 red_dear = {
@@ -17,9 +19,10 @@ red_dear = {
     'description': "A red deer painted with color pencils that was given to my by my grand parents. This drawing is tributed to them.",
     'image': 'red_dear.jpg',
     'url': 'red_dear.jpg',
-    'skills': ['Iconic', 'Nature', 'Colors']
+    'skills': ['Iconic', 'Nature', 'Colors'],
+    'titel_sv': 'Röd Hjort',
+    'description_sv': "En röd hjort målad med färgpennor som jag fick av mina morföräldrar. Den här teckningen är tillägnad dem."
 }
-
 
 colorful_girl = {
     'titel': 'Colorful girl',
@@ -27,27 +30,31 @@ colorful_girl = {
     'description': "Colorful girl holding her phone. Illustrating the cooler full yet sometimes inauthentic nature of online communication.",
     'image': 'colorful_girl.jpg',
     'url': 'colorful_girl.jpg',
-    'skills': ['Human',  'Colors', 'phone']
+    'skills': ['Human',  'Colors', 'phone'],
+    'titel_sv': 'Färgglad tjej',
+    'description_sv': "Färgglad tjej som håller i sin telefon. Illustrerar den svalare men ibland oäkta naturen av online-kommunikation."
 }
 
-
-eart_water_colors = {
+earth_water_colors = {
     'titel': 'Water Color Earth',
     'year': 2018,
     'description': "The earth painted with watercolors. The wonderful wet rock floating in space that we live on. <3",
     'image': 'earth_in_space_water_color.jpg',
     'url': 'earth_in_space_water_color.jpg',
-    'skills': ['Water Colors', 'Space', 'Earth']
+    'skills': ['Water Colors', 'Space', 'Earth'],
+    'titel_sv': 'Akvarelljord',
+    'description_sv': "Jorden målad med akvareller. Den underbara våta stenen som svävar i rymden som vi lever på. <3"
 }
-
 
 red_flower_triangel_base = {
     'titel': 'Red Flower',
     'year': 2024,
-    'description': "In the center is a circel contained by a squere contained by a triangel contained by a circle contained by red flower pedals illustrated on a paper.",
+    'description': "In the center is a circle contained by a square contained by a triangle contained by a circle contained by red flower petals illustrated on paper.",
     'image': 'blue_red_flower.jpg',
     'url': 'blue_red_flower.jpg',
-    'skills': ['Color', 'Red', 'Green', 'Geometry', 'Pastell']
+    'skills': ['Color', 'Red', 'Green', 'Geometry', 'Pastel'],
+    'titel_sv': 'Röd Blomma',
+    'description_sv': "I mitten finns en cirkel innesluten av en kvadrat innesluten av en triangel innesluten av en cirkel innesluten av röda blomblad illustrerade på papper."
 }
 
 split_eyes = {
@@ -56,7 +63,9 @@ split_eyes = {
     'description': "Forgotten exactly what I was thinking when I drew this.",
     'image': 'split_eyes.jpg',
     'url': 'split_eyes.jpg',
-    'skills': ['Color', 'Eyes', 'Trinity', 'Duality', 'Crayons']
+    'skills': ['Color', 'Eyes', 'Trinity', 'Duality', 'Crayons'],
+    'titel_sv': 'Delade ögon',
+    'description_sv': "Glömt exakt vad jag tänkte när jag ritade detta."
 }
 
 walking_dragon = {
@@ -65,7 +74,9 @@ walking_dragon = {
     'description': "Trying to figure out colors. In this image yellow are used as highlights and blue as shadows.",
     'image': 'walking_dragon.jpg',
     'url': 'walking_dragon.jpg',
-    'skills': ['Color', 'Dragon']
+    'skills': ['Color', 'Dragon'],
+    'titel_sv': 'Gående drake',
+    'description_sv': "Försöker lista ut färger. I den här bilden används gult som höjdpunkter och blått som skuggor."
 }
 
 line_one_line = {
@@ -74,17 +85,20 @@ line_one_line = {
     'description': "Line fractal filled with colors.",
     'image': 'line_one_line.jpg',
     'url': 'line_one_line.jpg',
-    'skills': ['Color', 'Line', 'Fractal']
+    'skills': ['Color', 'Line', 'Fractal'],
+    'titel_sv': 'Linje Ett Linje',
+    'description_sv': "Linje-fraktal fylld med färger."
 }
-
 
 painted_heart = {
     'titel': 'Painted Heart',
     'year': 2017,
-    'description': "Painted a heart by using water coolors.",
+    'description': "Painted a heart by using water colors.",
     'image': 'painted_heart.jpg',
     'url': 'painted_heart.jpg',
-    'skills': ['Color', 'hearth']
+    'skills': ['Color', 'heart'],
+    'titel_sv': 'Målat hjärta',
+    'description_sv': "Målade ett hjärta med vattenfärger."
 }
 
 dubbel_spiral = {
@@ -93,7 +107,9 @@ dubbel_spiral = {
     'description': "Dubbel Spiral",
     'image': 'dubbel_spiral.jpg',
     'url': 'dubbel_spiral.jpg',
-    'skills': ['Color', 'spiral']
+    'skills': ['Color', 'spiral'],
+    'titel_sv': 'Dubbel Spiral',
+    'description_sv': "Dubbel spiral"
 }
 
 castle_in_the_sky = {
@@ -102,7 +118,9 @@ castle_in_the_sky = {
     'description': "Castel in the sky",
     'image': 'castle_in_the_sky.jpg',
     'url': 'castle_in_the_sky.jpg',
-    'skills': ['Color', 'sky']
+    'skills': ['Color', 'sky'],
+    'titel_sv': 'Slottet på himlen',
+    'description_sv': "Slottet på himlen"
 }
 
 in_giving_we_receive = {
@@ -111,9 +129,10 @@ in_giving_we_receive = {
     'description': "In giving we receive",
     'image': 'in_giving_we_receive.jpg',
     'url': 'in_giving_we_receive.jpg',
-    'skills': ['Hands']
+    'skills': ['Hands'],
+    'titel_sv': 'Genom att ge får vi',
+    'description_sv': "Genom att ge får vi"
 }
-
 
 bird_back = {
     'titel': 'Bird Back',
@@ -121,7 +140,9 @@ bird_back = {
     'description': "Flying Away",
     'image': 'bird_back.jpg',
     'url': 'bird_back.jpg',
-    'skills': ['Bird']
+    'skills': ['Bird'],
+    'titel_sv': 'Fågel Rygg',
+    'description_sv': "Flyger bort"
 }
 
 fruit_and_light = {
@@ -130,84 +151,101 @@ fruit_and_light = {
     'description': "One Circle and six circles placed around it with common intersections with the one circle.",
     'image': 'fruit_and_light.jpg',
     'url': 'fruit_and_light.jpg',
-    'skills': ['Circels']
+    'skills': ['Circles'],
+    'titel_sv': 'Frukt av ljus',
+    'description_sv': "En cirkel och sex cirklar placerade runt den med gemensamma korsningar med den första cirkeln."
 }
 
 portal = {
     'titel': 'Portal',
     'year': 2024,
     'description': "A bit unfocused scribbling. Looks a bit like a framed portal",
-    'description_sve': "Lite ofocuserat kladdande. Liknar lite en inramad portal.",
+    'description_sv': "Lite ofocuserat kladdande. Liknar lite en inramad portal.",
     'image': 'portal.jpg',
     'url': 'portal.jpg',
-    'skills': ['Portal', 'painting']
+    'skills': ['Portal', 'painting'],
+    'titel_sv': 'Portal',
+    'description_sv': "Lite ofokuserat klotter. Ser lite ut som en inramad portal."
 }
 
-
 butterfly = {
-	'titel': 'butterfly',
-	'year': 2018,
-	'description': "A sketch of a butterfly",
-	'url': 'butterfly_black_white.jpg',
-	'skills': ['sketch']
+    'titel': 'Butterfly',
+    'year': 2018,
+    'description': "A sketch of a butterfly",
+    'url': 'butterfly_black_white.jpg',
+    'skills': ['sketch'],
+    'titel_sv': 'Fjäril',
+    'description_sv': "En skiss av en fjäril"
 }
 
 another_abstract = {
-	'titel': 'Another Abstract',
-	'year': 2024,
-	'description': "It's just another abstarct.",
-	'url': 'Another_Abstract.jpg',
+    'titel': 'Another Abstract',
+    'year': 2024,
+    'description': "It's just another abstract.",
+    'url': 'Another_Abstract.jpg',
     'image': 'Another_Abstract.jpg',
-	'skills': ['oil pastel']
+    'skills': ['oil pastel'],
+    'titel_sv': 'Ytterligare ett abstrakt',
+    'description_sv': "Det är bara ett annat abstrakt."
 }
 
 cap_girl = {
-	'titel': 'Cap Girl',
-	'year': 2018,
-	'description': "Girl with a cap.",
-	'description_sv': "Tjej med keps.",
-	'url': 'cap_girl.jpg',
-	'skills': ['crayons']
+    'titel': 'Cap Girl',
+    'year': 2018,
+    'description': "Girl with a cap.",
+    'description_sv': "Tjej med keps.",
+    'url': 'cap_girl.jpg',
+    'skills': ['crayons'],
+    'titel_sv': 'Kepsflicka',
+    'description_sv': "Flicka med en keps."
 }
 
 guy_with_glasses = {
-	'titel': 'Guy with Glasses',
-	'year': 2024,
-	'description': "Guy with glasses painted with Ipad.",
-	'description_sv': "Kille med keps målad på en Ipad.",
-	'url': 'guy_with_glasses.png',
-	'skills': ['digirtal', 'portarait', 'human', 'glasses']
+    'titel': 'Guy with Glasses',
+    'year': 2024,
+    'description': "Guy with glasses painted with iPad.",
+    'description_sv': "Kille med glasögon målad med iPad.",
+    'url': 'guy_with_glasses.png',
+    'skills': ['digital', 'portrait', 'human', 'glasses'],
+    'titel_sv': 'Kille med glasögon',
+    'description_sv': "Kille med glasögon målad med iPad."
 }
 
 girl_with_hands = {
-	'titel': 'Girl with Hands',
-	'year': 2023,
-	'description': "Girl with hands.",
-	'description_sv': "Tjej med händer.",
-	'url': 'girl_with_hands.png',
-	'skills': ['digirtal', 'portarait', 'human', 'hands']
+    'titel': 'Girl with Hands',
+    'year': 2023,
+    'description': "Girl with hands.",
+    'description_sv': "Tjej med händer.",
+    'url': 'girl_with_hands.png',
+    'skills': ['digital', 'portrait', 'human', 'hands'],
+    'titel_sv': 'Tjej med händer',
+    'description_sv': "Tjej med händer."
 }
 
 jinx = {
-	'titel': 'Jinx',
-	'year': 2022,
-	'description': "Jinx from league of legends.",
-	'description_sv': "Jinx från league of legends.",
-	'url': 'jinx.png',
-	'skills': ['digirtal', 'portarait', 'human']
+    'titel': 'Jinx',
+    'year': 2022,
+    'description': "Jinx from League of Legends.",
+    'description_sv': "Jinx från League of Legends.",
+    'url': 'jinx.png',
+    'skills': ['digital', 'portrait', 'human'],
+    'titel_sv': 'Jinx',
+    'description_sv': "Jinx från League of Legends."
 }
 
 yennefer = {
-	'titel': 'Yennefer',
-	'year': 2022,
-	'description': "Yennefer from the witcher.",
-	'description_sv': "Yennefer från the witcher.",
-	'url': 'yennefer_the_witcher.png',
-	'skills': ['digirtal', 'portarait', 'human']
+    'titel': 'Yennefer',
+    'year': 2022,
+    'description': "Yennefer from The Witcher.",
+    'description_sv': "Yennefer från The Witcher.",
+    'url': 'yennefer_the_witcher.png',
+    'skills': ['digital', 'portrait', 'human'],
+    'titel_sv': 'Yennefer',
+    'description_sv': "Yennefer från The Witcher."
 }
 
 #fruit_and_light ,
-art_projects = [yennefer, jinx, girl_with_hands, guy_with_glasses, cap_girl, butterfly, another_abstract, portal, red_flower_triangel_base, wolf_drawing, eart_water_colors, 
+art_projects = [yennefer, jinx, girl_with_hands, guy_with_glasses, cap_girl, butterfly, another_abstract, portal, red_flower_triangel_base, wolf_drawing, earth_water_colors, 
                 line_one_line ,walking_dragon, split_eyes, colorful_girl,dubbel_spiral, 
                 in_giving_we_receive, red_dear, castle_in_the_sky, painted_heart, bird_back]
 
